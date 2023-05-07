f6e8b3b3d2dc97e4c003317363f733364a4b6545
Pavel Gorodilov <NKtym@users.noreply.github.com>
2023-05-07 22:42:36 +0700

    pohu

--- quize_run.h
+++ quize_run.h
@@ -19,5 +19,5 @@ int write_name(struct user* users);
 int test_selection(struct user* users);
 int percentage_of_completion(struct answers* answer,struct user* users,int questionCount);
 int сonclusion_quest(struct quizz* quiz,int questionCount);
-struct answers answer_quest(struct answers* results,int optionCount,char answer);
+struct answers answer_quest(struct answers* results,int optionCount);
 int Validation(struct quizz* quiz,struct answers* results,int questionCount,int optionCount);
